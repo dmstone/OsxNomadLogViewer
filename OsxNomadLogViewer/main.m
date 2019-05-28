3c89c4d343854535cd20a075a534e7d8c5455611
dougstone <dstone229@gmail.com>
2019-05-27 21:33:23 -0400

    Added command line option

--- OsxNomadLogViewer/main.m
+++ OsxNomadLogViewer/main.m
@@ -5,6 +5,8 @@
 //  Created by Doug Stone on 5/27/19.
 //  Copyright © 2019 Doug Stone. All rights reserved.
 //
+// Build:
+// gcc -framework Foundation main.m -o test1
 
 #import <Foundation/Foundation.h>
 
@@ -12,6 +14,30 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        const char * ipAddress;
+        
+        for (int x=0; x<argc; x++) {
+            NSLog(@"Arg %d %s\n", x, argv[x]);
+            
+            const char * str = argv[x];
+            
+            switch(str[0]) {
+                case '-':
+                    if (strlen(str) >= 2) {
+                        switch (str[1]) {
+                            case 'a':
+                                x++;
+                                if (x < argc) {
+                                    ipAddress = argv[x];
+                                    NSLog(@"ipaddress = %s\n", ipAddress);
+                                }
+                                break;
+                        };
+                    }
+                break;
+            };
+        }
     }
     return 0;
 }
