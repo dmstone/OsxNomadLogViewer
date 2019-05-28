a6e1c5249d01813d33eda9aba22c1b9df396c0ed
dougstone <dstone229@gmail.com>
2019-05-27 21:09:51 -0400

    Initial base code

---
+++ OsxNomadLogViewer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OsxNomadLogViewer
+//
+//  Created by Doug Stone on 5/27/19.
+//  Copyright © 2019 Doug Stone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
